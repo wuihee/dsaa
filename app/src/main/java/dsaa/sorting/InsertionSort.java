a17bac484b47e4d61eaac027889a5abb0ad3c98b
wuihee <wuihee@gmail.com>
2024-06-25 14:50:07 +0800

    Updated repo

---
+++ app/src/main/java/dsaa/sorting/InsertionSort.java
@@ -0,0 +1,11 @@
+package dsaa.sorting;
+
+public class InsertionSort {
+    public int getInt() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ app/src/test/java/dsaa/sorting/InsertionSortTest.java
@@ -0,0 +1,11 @@
+package dsaa.sorting;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InsertionSortTest {
+    @Test void testTest() {
+        InsertionSort sort = new InsertionSort();
+        assertEquals(1, sort.getInt());
+    }   
+}
